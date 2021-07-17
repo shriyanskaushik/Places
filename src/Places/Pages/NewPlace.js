d0a93773a37b2fbde065657cc3141496ab16c77f
Shriyans Kaushik <shriyanskaushik4@gmail.com>
2021-07-17 17:09:32 +0530

    Validators

--- src/Places/Pages/NewPlace.js
+++ src/Places/Pages/NewPlace.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React,{useCallback} from 'react'
 
-import { VALIDATOR_REQUIRE } from "../../Shared/util/validators";
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../Shared/util/validators";
 import { Input } from "../../Shared/Components/FormElements/Input";
 import './NewPlace.css';
 
 export const NewPlace = () => {
+    const titleInputHandler = useCallback((id, value, isValid) => {
+
+    }, []);
+    const descriptionInputHandler = useCallback((id, value, isValid) => {
+
+    }, []);
+
     return (
         <form className = "place-form">
-            <Input type = "text" label = "Title" element = "input" errorText = "Please enter the title first." placeholder="Enter Title" validators = {[VALIDATOR_REQUIRE()]} />
+            <Input type = "text" label = "Title" element = "input" errorText = "Please enter the title first." placeholder="Enter Title" validators = {[VALIDATOR_REQUIRE()]} onInput = {titleInputHandler} id = "title" />
+            <Input label = "Description" element = "textarea" errorText = "Please enter a valid description (atleast 5 characters)." placeholder="Enter Description" validators = {[ VALIDATOR_MINLENGTH(5)]} onInput = {descriptionInputHandler} id = "description" />
         </form>
     )
 }
--- src/Shared/Components/FormElements/Input.js
+++ src/Shared/Components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react'
+import React, {useReducer,useEffect} from 'react'
 
 import { validate } from "../../util/validators";
 import './Input.css'
@@ -28,6 +28,13 @@ export const Input = (props) => {
         dispatch({type:'CHANGE', val : event.target.value, validators : props.validators});
     };
 
+    const {id, onInput} = props;
+    const { value, isValid } = inputState;
+
+    useEffect(() => {
+        onInput(id, value, isValid);
+    }, [id, value, isValid, onInput]);
+
     const touchHandler = () => {
         dispatch({type : 'TOUCH'});
     }
@@ -43,7 +50,7 @@ export const Input = (props) => {
             value = {inputState.value}
         />
         ) : (
-        <textarea id = {props.id} rows = {props.rows || 3} onChange = {changeHandler} onBlur = {touchHandler} value = {inputState.value} />
+        <textarea id = {props.id} rows = {props.rows || 3} onChange = {changeHandler} onBlur = {touchHandler} value = {inputState.value} placeholder = {props.placeholder} />
         );
 
     return (
